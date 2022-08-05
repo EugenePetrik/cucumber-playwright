82716f67e00473c432b79441c05b719551715081
Eugene Petrik <eugeniy.petrik@gmail.com>
2022-08-05 08:17:49 +0300

    Add tests for tables

---
+++ e2e/src/step-definitions/assertions/verify-table-value.ts
@@ -0,0 +1,33 @@
+import { When, DataTable } from '@cucumber/cucumber';
+import { ScenarioWorld } from '../setup/world';
+import { getElementLocator } from '../../support/web-element-helper';
+import { ElementKey } from '../../env/global';
+import { waitFor } from '../../support/wait-for-behavior';
+
+When(
+    /^the "([^"]*)" table should( not)? equal the following:$/,
+    async function (this: ScenarioWorld, elementKey: ElementKey, negate: boolean, dataTable: DataTable) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`The ${elementKey} table should ${negate ? 'not' : ''} equal the following:`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        const dataBefore = await page.$$eval(elementIdentifier + ' tbody tr', rows => {
+            return rows.map(row => {
+                const cells = row.querySelectorAll('td');
+                return Array.from(cells).map(cell => cell.textContent);
+            });
+        });
+
+        console.log('HTML table ', JSON.stringify(dataBefore));
+        console.log('Cucumber table ', JSON.stringify(dataTable.raw()));
+
+        await waitFor(async () => {
+            return (JSON.stringify(dataBefore) === JSON.stringify(dataTable.raw())) === !negate;
+        });
+    },
+);

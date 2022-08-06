788e7e08176edf5103541ce95898effb2a2a1e7a
Eugene Petrik <eugeniy.petrik@gmail.com>
2022-08-06 20:47:31 +0300

    Add tests for avatars

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -2,6 +2,7 @@ import { Then } from '@cucumber/cucumber';
 import { getElementLocator } from '../../support/web-element-helper';
 import { ScenarioWorld } from '../setup/world';
 import { waitFor } from '../../support/wait-for-behavior';
+import { ElementKey } from '../../env/global';
 
 Then(/^the "([^"]*)" should( not)? be displayed$/, async function (this: ScenarioWorld, elementKey: string, negate: boolean) {
     const {
@@ -18,3 +19,42 @@ Then(/^the "([^"]*)" should( not)? be displayed$/, async function (this: Scenari
         return isElementVisible === !negate;
     });
 });
+
+Then(
+    /^the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" "([^"]*)" should( not)? be displayed$/,
+    async function (this: ScenarioWorld, elementPosition: string, elementKey: ElementKey, negate: boolean) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`The ${elementPosition} ${elementKey} should ${negate ? 'not' : ''} be displayed`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+        const index = Number(elementPosition.match(/\d/g)?.join('')) - 1;
+
+        await waitFor(async () => {
+            const isElementVisible = (await page.$(`${elementIdentifier}>>nth=${index}`)) != null;
+            return isElementVisible === !negate;
+        });
+    },
+);
+
+Then(
+    /^I should( not)? see "(\d*)" "([^"]*)" displayed$/,
+    async function (this: ScenarioWorld, negate: boolean, count: string, elementKey: ElementKey) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`I should ${negate ? 'not' : ''} see ${count} ${elementKey} displayed`);
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            const element = await page.$$(elementIdentifier);
+            return (Number(count) === element.length) === !negate;
+        });
+    },
+);
